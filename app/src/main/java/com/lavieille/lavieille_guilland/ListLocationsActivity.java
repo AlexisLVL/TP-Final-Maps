c12259dd3a48d7b012f8f8cfa2abac4fa1445253
LAVIEILLE Alexis <alexis.lavieille@etu.univ-lyon1.fr>
2023-03-14 14:35:31 +0100

    Work: fix ListLocationActivity

--- app/src/main/java/com/lavieille/lavieille_guilland/ListLocationsActivity.java
+++ app/src/main/java/com/lavieille/lavieille_guilland/ListLocationsActivity.java
@@ -7,47 +7,97 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 import com.lavieille.lavieille_guilland.adapter.LocationsAdapter;
 import com.lavieille.lavieille_guilland.entity.Location;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ListLocationsActivity extends AppCompatActivity {
-
-    public ArrayList<Location> arrayOfLocations = new ArrayList<Location>();
+    public ArrayList<Location> arrayOfLocations = new ArrayList<>();
     public LocationsAdapter adapter;
-
-    ActivityResultLauncher<Intent> mainActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK){
-                    Intent data = result.getData();
-                    if (data != null){
-                        Location newLocation = (Location) data.getSerializableExtra("rep");
-                        arrayOfLocations.add(newLocation);
-                        adapter.notifyDataSetChanged();
-                    }
-                }
-            }
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_locations);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        final Button button = findViewById(R.id.button);
-        adapter = new LocationsAdapter(this, arrayOfLocations);
+        Button backButton = findViewById(R.id.BackButton);
+        Button mapButton = findViewById(R.id.MapButton);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
 
         ListView listView = findViewById(R.id.lvLocations);
+        adapter = new LocationsAdapter(this, arrayOfLocations);
         listView.setAdapter(adapter);
 
-        button.setOnClickListener(view -> {
-            mainActivityResultLauncher.launch(intent);
+        executor.execute(() -> {
+            try {
+                URL url = new URL   ("http://tp3.hexteckgate.ga/api.php");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String jsonData = input.readLine();
+                    input.close();
+
+                    ArrayList<LinkedTreeMap<String, String>> jsonDataTable = makeIntoTable(jsonData);
+
+                    handler.post(() -> {
+                        for (LinkedTreeMap<String, String> tmp :
+                                jsonDataTable) {
+                            Location newLocation = new Location(tmp.get("title"), tmp.get("address"), tmp.get("coordinates"), tmp.get("description"),tmp.get("note"));
+                            arrayOfLocations.add(newLocation);
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                Log.e("Error : connexion", e.getLocalizedMessage());
+            }
         });
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        System.out.println("Short Click");
+                    }
+                });
+
+        listView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        System.out.println("long Click");
+                        return false;
+                    }
+                }
+        );
+
+        backButton.setOnClickListener(view -> {
+            //Intent intent = new Intent(this, MainActivity.class);
+            //startActivity(intent);
+        });
+    }
+
+    private ArrayList<LinkedTreeMap<String, String>> makeIntoTable(String json){
+        return new Gson().fromJson(json, ArrayList.class);
     }
 }
--- app/src/main/java/com/lavieille/lavieille_guilland/MainActivity.java
+++ app/src/main/java/com/lavieille/lavieille_guilland/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lavieille.lavieille_guilland;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,72 +36,32 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<Location> arrayOfLocations = new ArrayList<>();
     public LocationsAdapter adapter;
 
+    ActivityResultLauncher<Intent> mainActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    if (data != null){
+                        Location newLocation = (Location) data.getSerializableExtra("rep");
+                        arrayOfLocations.add(newLocation);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_locations);
 
-        Button backButton = findViewById(R.id.BackButton);
-        Button mapButton = findViewById(R.id.MapButton);
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
+        Intent intent = new Intent(this, MainActivity.class);
+        final Button button = findViewById(R.id.button);
+        adapter = new LocationsAdapter(this, arrayOfLocations);
 
         ListView listView = findViewById(R.id.lvLocations);
-        adapter = new LocationsAdapter(this, arrayOfLocations);
         listView.setAdapter(adapter);
 
-        executor.execute(() -> {
-            try {
-                URL url = new URL   ("http://tp3.hexteckgate.ga/api.php");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
-                    BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String jsonData = input.readLine();
-                    input.close();
-
-                    ArrayList<LinkedTreeMap<String, String>> jsonDataTable = makeIntoTable(jsonData);
-
-                    handler.post(() -> {
-                        for (LinkedTreeMap<String, String> tmp :
-                        jsonDataTable) {
-                            Location newLocation = new Location(tmp.get("title"), tmp.get("address"), tmp.get("coordinates"), tmp.get("description"),tmp.get("note"));
-                            arrayOfLocations.add(newLocation);
-                            adapter.notifyDataSetChanged();
-                        }
-                    });
-                }
-            } catch (Exception e) {
-                Log.e("Error : connexion", e.getLocalizedMessage());
-            }
-        });
-
-        listView.setOnItemClickListener(
-            new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    System.out.println("Short Click");
-                }
-        });
-
-        listView.setOnItemLongClickListener(
-            new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    System.out.println("long Click");
-                    return false;
-                }
-            }
-        );
-
-        backButton.setOnClickListener(view -> {
-            //Intent intent = new Intent(this, MainActivity.class);
-            //startActivity(intent);
-        });
-    }
-
-    private ArrayList<LinkedTreeMap<String, String>> makeIntoTable(String json){
-        return new Gson().fromJson(json, ArrayList.class);
+        button.setOnClickListener(view -> mainActivityResultLauncher.launch(intent));
     }
 }
